fd24f1aad4f5670e058ffaf674be9489e3eaffc1
Ken_Ch <condoricristian866@gmail.com>
2026-04-24 23:58:52 -0500

    ENDPOINT con metodo PUT finalizado

---
+++ models/clienteModel.js
@@ -0,0 +1,10 @@
+class Cliente {
+    constructor(id, nombre, correo, telefono) {
+        this.id = id;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.telefono = telefono
+    }
+}
+
+module.exports = Cliente;
