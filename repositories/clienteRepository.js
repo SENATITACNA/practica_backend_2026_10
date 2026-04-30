33c1062ed145ba7cf0b2001236538276f27d2ebb
Miraval Palomino <mirav@DESKTOP-HF5VL4Q>
2026-04-30 10:39:46 -0500

    feat: endpoint PUT categorias

--- repositories/clienteRepository.js
+++ repositories/clienteRepository.js
@@ -1,15 +1,36 @@
-const db = require("../config/db");
-const eliminarCliente = (id, callback) => {
- const sql = "DELETE FROM clientes WHERE id = ?";
- db.query(sql, [id], (err, resultado) => {
-  if (err) {
-   callback(err, null);
-  } else {
-   callback(null, resultado);
-  }
- });
+const db = require('../config/db');
+
+// 🔍 Buscar por ID
+const getById = (id) => {
+    return new Promise((resolve, reject) => {
+
+        db.query('SELECT * FROM clientes WHERE id = ?', [id], (err, results) => {
+            if (err) reject(err);
+            else resolve(results[0]);
+        });
+
+    });
 };
 
-module.exports = { 
-    eliminarCliente 
+// ✏️ Actualizar
+const update = (id, cliente) => {
+    return new Promise((resolve, reject) => {
+
+        const query = `
+            UPDATE clientes
+            SET nombre = ?, email = ?
+            WHERE id = ?
+        `;
+
+        db.query(query, [cliente.nombre, cliente.email, id], (err, result) => {
+            if (err) reject(err);
+            else resolve(result);
+        });
+
+    });
+};
+
+module.exports = {
+    getById,
+    update
 };
