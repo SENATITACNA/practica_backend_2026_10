72ccde94bf29f55571433dc73d59b4a59969fe2a
freddyrc89 <freddyrc89@hotmail.com>
2026-05-02 18:07:53 -0500

    correccion de actualizar categoria

--- repositories/categoriaRepository.js
+++ repositories/categoriaRepository.js
@@ -1,5 +1,17 @@
 const db = require("../config/db");
 
+const actualizarCategoria = (id, datos, callback) => {
+  const sql = "UPDATE categorias SET nombre = ?, descripcion = ? WHERE id = ?";
+
+  db.query(sql, [datos.nombre, datos.descripcion, id], (err, resultado) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, resultado);
+    }
+  });
+};
+
 const crearCategoria = (categoria, callback) => {
   const sql = "INSERT INTO categorias (nombre, descripcion) VALUES (?, ?)";
   db.query(sql, [categoria.nombre, categoria.descripcion], (err, resultado) => {
@@ -25,5 +37,6 @@ const listarCategorias = (callback) => {
 
 module.exports = {
   actualizarCategoria,
-  listarCategorias
+  listarCategorias,
+  crearCategoria
 };
